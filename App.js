ef7beedbb32e0a9f1b3327bc31594ff2f6bac1c8
wajdem <wajdi.abuarqob@gmail.com>
2023-08-22 21:52:18 +0300

    done wajde

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet} from 'react-native';
 import StackNavigator from './navigator/StackNavigator';
-import { AppLoading } from 'expo';
-import { useFonts } from 'expo-font';
+
+
+
 export default function App() {
   return (
     <>
